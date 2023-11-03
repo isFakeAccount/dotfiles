ecbc6033508ff9541fef0e067eec5e4b1ee96b18
Yoshikage Kira <trevorphillips@gmx.us>
2023-11-03 01:32:59 -0400

    Added alias.sh

---
+++ aliases.sh
@@ -0,0 +1,17 @@
+alias ssha='eval "$(ssh-agent -s)" && ssh-add'
+
+tmux_nvim() {
+    local session_name=$(basename "$(pwd)")
+    if tmux has-session -t "$session_name" 2>/dev/null; then
+        tmux attach-session -t "$session_name"
+    else
+        tmux new-session -s "$session_name" -n nvim -d
+        tmux send-keys -t "$session_name" "nvim ." C-m
+        tmux attach-session -t "$session_name"
+    fi
+}
+
+activate_venv() {
+    folder_name=${1:-"venv"}
+    source "$folder_name"/bin/activate
+}
