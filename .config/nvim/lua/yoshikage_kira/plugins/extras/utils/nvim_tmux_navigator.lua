8ebfd483998f5f0e60d20f8712e9a039ff01e4fb
Yoshikage Kira <trevorphillips@gmx.us>
2023-12-02 14:42:55 -0500

    Added keymaps to easily resize windows in nvim
    Added keymaps to easily resize windows in nvim

--- .config/nvim/lua/yoshikage_kira/plugins/extras/utils/nvim_tmux_navigator.lua
+++ .config/nvim/lua/yoshikage_kira/plugins/extras/utils/nvim_tmux_navigator.lua
@@ -13,5 +13,12 @@ return {
         vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
         vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
         vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+        
+	-- Resize windows using Alt + hjkl
+	vim.keymap.set('n', '<M-h>', ':vertical resize -2<CR>', { noremap = true, silent = true })
+	vim.keymap.set('n', '<M-l>', ':vertical resize +2<CR>', { noremap = true, silent = true })
+	vim.keymap.set('n', '<M-j>', ':resize -2<CR>', { noremap = true, silent = true })
+	vim.keymap.set('n', '<M-k>', ':resize +2<CR>', { noremap = true, silent = true })
+
     end
 }
