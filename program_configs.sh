3cd165f10e8d534e3a81c129baaf407ee96e120a
Yoshikage Kira <trevorphillips@gmx.us>
2024-01-27 15:01:03 -0500

    Removed bob from dotfiles

--- program_configs.sh
+++ program_configs.sh
@@ -5,9 +5,6 @@ eval "$(pyenv init -)"
 # Cargo: Rust build system
 source "$XDG_DATA_HOME/cargo/env"
 
-# Bob: nvim version manager config
-export PATH="$XDG_DATA_HOME/bob/nvim-bin:$PATH"
-
 # Nvm: Node version manager
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
