932f6757d4589f97d57e15e84218ae096ae13e92
Yoshikage Kira <trevorphillips@gmx.us>
2024-02-12 15:07:45 -0500

    Git config is now created dynamically

--- bash_configs/xdg_setup.py
+++ bash_configs/xdg_setup.py
@@ -1,12 +1,33 @@
 from os import getenv
 from pathlib import Path
 from typing import NamedTuple
+from git import GitConfigParser
 
 
 class XDGPath(NamedTuple):
     path: Path
     is_file: bool
     mode: int
+
+
+def create_global_gitconfig():
+    programming_dir = Path.home() / "Programming"
+    ifinclude_paths: list[Path] = []
+    for subdir in programming_dir.iterdir():
+        ifinclude_paths.append(subdir.resolve())
+
+    git_config_path = (
+        Path(__file__).parent.parent.resolve() / ".config" / "git" / "config"
+    )
+    with GitConfigParser(git_config_path, read_only=False) as config_parser:
+        config_parser.set_value("init", "defaultBranch", "master")
+
+        for ifinclude_path in ifinclude_paths:
+            config_parser.set_value(
+                f'includeIf "gitdir:{ifinclude_path}/"',
+                "path",
+                f"{ifinclude_path}/.gitconfig",
+            )
 
 
 def create_xdg_structure(xdg_paths: list[XDGPath]):
@@ -28,9 +49,11 @@ def main():
         XDGPath(path=xdg_state_home / "bash" / "history", is_file=True, mode=0o600),
         XDGPath(path=xdg_state_home / "less" / "history", is_file=True, mode=0o600),
         XDGPath(path=xdg_data_home / "gnupg", is_file=False, mode=0o700),
+        XDGPath(path=xdg_data_home / "nvm", is_file=False, mode=0o700),
     ]
 
     create_xdg_structure(xdg_paths)
+    create_global_gitconfig()
 
 
 if __name__ == "__main__":
