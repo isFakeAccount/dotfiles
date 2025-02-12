6d281b5301387fef8ae175407e2fc2f6686f3f8b
Yoshikage Kira <8bit_yoshikage_kira@proton.me>
2025-02-12 01:19:54 -0500

    Add UV config to prefer system python

--- home/dot_config/bash/program_configs.sh
+++ home/dot_config/bash/program_configs.sh
@@ -6,3 +6,6 @@ source "$XDG_DATA_HOME/cargo/env"
 # Nvm: Node version manager
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
+# UV: Python version manager
+export UV_PYTHON_PREFERENCE='system'
